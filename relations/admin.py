54d1cfedf035bbd6d9cd0a04b467faed863d9ff7
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-08-23 19:08:11 +0300

    models and relations are OK

--- relations/admin.py
+++ relations/admin.py
@@ -4,3 +4,5 @@ from .models import *
 
 admin.site.register(Creator)
 admin.site.register(Language)
+admin.site.register(Framework)
+admin.site.register(Developer)
--- relations/models.py
+++ relations/models.py
@@ -12,6 +12,7 @@ class Creator(models.Model):
         ordering = ['first_name']
 
 
+# one2one relation
 class Language(models.Model):
     name = models.CharField(max_length=50)
     creator = models.OneToOneField(Creator, on_delete=models.CASCADE)
@@ -20,4 +21,49 @@ class Language(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.creator}"
+
+
+    class Meta:
+        ordering = ['name']
+
+
+'''
+on_delete properties:
+
+# CASCADE -> if primary deleted, delete forein too.
+# SET_NULL -> if primary deleted, set foreign to NULL. (null=True)
+# SET_DEFAULT -> if primary deleted, set foreing to DEFAULT value. (default='Value")
+# DO NOTHING -> if primary deleted, do nothing.
+# PROTECT -> if foreign is exist, can not delete primary.
+
+'''
+
+# many2one relation : piyasada default en çok kullanılan
+class Framework(models.Model):
+    name = models.CharField(max_length=50)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.language}"
+
+
+    class Meta:
+        ordering = ['name']
+
+
+# many2many relation
+class Developer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    framework = models.ManyToManyField(Framework) # not use on delete!!!
+
+
+    def __str__(self):
+        return f"{self.first_name} - {self.last_name}"
+
+
+    class Meta:
+        ordering = ['first_name']
+    
+    
     
